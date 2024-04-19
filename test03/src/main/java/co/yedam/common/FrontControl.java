73545fb892b90777e3795a0e976659a110995ccb
admin <admin@YD05-09>
2024-04-19 12:40:53 +0900

    2024/04/19

--- test03/src/main/java/co/yedam/common/FrontControl.java
+++ test03/src/main/java/co/yedam/common/FrontControl.java
@@ -1,8 +1,26 @@
 package co.yedam.common;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 
 public class FrontControl extends HttpServlet{
 	
-	
+	//url pattern - 실행서블릿, 관리.
+		Map<String, Control> map;
+		
+		public FrontControl(){
+			map = new HashMap<>();
+		}
+		
+		@Override
+		public void init(ServletConfig config) throws ServletException {
+			
+			//map ~~~~
+			
+		}
+		
 }
