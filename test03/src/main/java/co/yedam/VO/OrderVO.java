50302990510ebb5c009740f7b2a7176032434b02
admin <admin@yedam.com>
2024-04-22 09:42:16 +0900

    0422

--- test03/src/main/java/co/yedam/VO/OrderVO.java
+++ test03/src/main/java/co/yedam/VO/OrderVO.java
@@ -8,7 +8,7 @@ import lombok.Data;
 public class OrderVO {
 	private int orderNo;  // sequence
 	private int clothNo;  // 옷번호
-	private String uidOrdoer; //user id
+	private String uidOrder; //user id
 	private int orderCnt; //주문 개수
 	private Date ordDate; //주문날짜
 	private int ordPirce; //주문금액
